87c760664f0978c89e0cc837869b66e7463eca93
spleasCZ <matamusil@email.cz>
2025-04-07 12:58:17 +0200

    gut

--- searching.py
+++ searching.py
@@ -1,3 +1,4 @@
+import json
 import os
 
 # get current working directory path
@@ -11,7 +12,21 @@ def read_data(file_name, field):
     :param field: (str), field of a dict to return
     :return: (list, string),
     """
+    if field not in("unordered_numbers", "ordered_numbers", "dna_sequence"):
+        return None
+
     file_path = os.path.join(cwd_path, file_name)
+
+    with open(file_path, "r") as json_file:
+        seq = json.load(json_file)
+    return seq[field]
+
+def  linear_search(sekvence, hledane_cislo):
+    vysledek = {seznam_pozic, pocet_vyskytu}
+
+
+
+    return vysledek
 
 
 def main():
